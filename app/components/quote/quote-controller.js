85a6592c0b49b683cf8039efc403bf1fb8bb328c
Meredith Ganz <2meredithganz@gmail.com>
2016-10-17 10:02:36 -0600

    push

--- app/components/quote/quote-controller.js
+++ app/components/quote/quote-controller.js
@@ -13,7 +13,7 @@
 			template = `
 			<div class="quoteSect">
 			<div id="quotewa">
-			<h5 class="text-center"><em>${qData.quote}</em></h5>
+			<h6 class="text-center"><em>${qData.quote}</em></h6>
 			<span id="author" class="text-center">${qData.author}</span>
 			</div>
 			</div>
